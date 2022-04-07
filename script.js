92b0da350f265c720ad6612686690553c312267c
austin-showen <austin.showen@gmail.com>
2022-04-07 16:13:16 -0700

    Add functionality to reset button

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const container = document.querySelector(".container");
-const size = 16;
+const startSize = 16;
 
-for (i = 0; i < size; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (j = 0; j < size; j++) {
-        const cell = document.createElement("span");
-        cell.classList.add("cell");
-        row.appendChild(cell);
+function draw(size) {
+    for (i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (j = 0; j < size; j++) {
+            const cell = document.createElement("span");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
     }
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.addEventListener("mouseenter", onHover));
 }
 
 function onHover() {
     this.classList.add("active");
 }
 
-const cells = document.querySelectorAll(".cell");
-console.log(cells);
-cells.forEach(cell => cell.addEventListener("mouseenter", onHover));
+function reset() {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => row.remove());
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.classList.remove("active"));
+    let newSize = 101;
+    while (newSize > 100 || newSize < 1) {
+        newSize = prompt("Enter a new grid size between 1 and 100.")
+    }
+    draw(newSize);
+}
+
+draw(startSize);
+
+
+
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", reset)
